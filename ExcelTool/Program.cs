4ddf19d748eea1df8f7ef75ae38e89db8591983d
mowenming <mowenming@gmail.com>
2017-08-04 17:26:56 +0800

    增加台头注释说明

--- ExcelTool/Program.cs
+++ ExcelTool/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +11,54 @@ namespace ExcelTool
 {
     class Program
     {
+        private const string txtPath = @"D:\data\txt";
+        private const string txtPostFix = ".txt";
+        private const int dataDistance = 5;
+        private const int tabDistance = 8;
+
         static void Main(string[] args)
         {
+            string fileName = Path.GetFileNameWithoutExtension(args[0]);     //返回不带扩展名的文件名 
+            string savepath = args[1];
+            string savefile = args[2];
+            Program p = new Program();
+            XlsxReader xr = new XlsxReader();
+            string errorString;
+            DataSet dataset = xr.ReadXlsxFile(args[0], out errorString);
+            p.ExportToTxt(dataset, savepath + @"\"+fileName+savefile);
+        }
+        /// <summary>
+        /// 导出
+        /// </summary>
+        /// <param name="ds"></param>
+        /// <param name="PathStr">完整路径</param>
+        public void ExportToTxt(DataSet ds, string PathStr)
+        {
+            StreamWriter writer = new StreamWriter(PathStr, false, new UTF8Encoding(false));
+
+           
+            int n = 0;
+            writer.WriteLine("%%配置导表自动生成，请不要随意手动修改！！！");
+            writer.WriteLine("%%配置导表自动生成，请不要随意手动修改！！！");
+            writer.WriteLine("%%配置导表自动生成，请不要随意手动修改！！！");
+            foreach (DataRow mDr in ds.Tables[0].Rows)
+            {
+                if(n>=5)
+                {
+                    string s = "-define(";
+
+                    s =s+ mDr[0].ToString() +","+ mDr[1].ToString()+").    %%" + mDr[2].ToString();
+                    writer.WriteLine(s);
+      
+                }
+                else
+                {
+                    n++;
+                }
+               
+            }
+            writer.Flush();
+            writer.Close();
         }
     }
 }
