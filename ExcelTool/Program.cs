4caaeb1162914497683fff1647e91bb7789e926c
mowenming <mowenming@gmail.com>
2017-08-09 11:48:39 +0800

    完善提示颜色

--- ExcelTool/Program.cs
+++ ExcelTool/Program.cs
@@ -18,19 +18,33 @@ namespace ExcelTool
 
         static void Main(string[] args)
         {
-            string fileName = Path.GetFileNameWithoutExtension(args[0]);     //返回不带扩展名的文件名 
-            string savepath = args[1];
-            string savefile = args[2];
-            Program p = new Program();
-            XlsxReader xr = new XlsxReader();
-            string errorString;
-            DataSet dataset = xr.ReadXlsxFile(args[0], out errorString);
-            p.ExportToTxt(dataset, savepath + @"\"+fileName+savefile);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("服务端文件{0}{1}导出完毕,存放目录为：{2}",fileName,savefile,savepath);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("---------------记得通知服务端同事进行处理---------------");
-            Console.ReadLine();
+            if (args != null)
+            {
+                if(args[0]== "ReadLine")
+                {
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("---------若有修改以上xlsx文件,需通知服务端进行处理。若无修改，请忽略---------");
+                    Console.ReadLine();
+                    Console.ResetColor(); //将控制台的前景色和背景色设为默认值
+                }
+                else
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(args[0]);     //返回不带扩展名的文件名 
+                    string savepath = args[1];
+                    string savefile = args[2];
+                    Program p = new Program();
+                    XlsxReader xr = new XlsxReader();
+                    string errorString;
+                    DataSet dataset = xr.ReadXlsxFile(args[0], out errorString);
+                    p.ExportToTxt(dataset, savepath + @"\" + fileName + savefile);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("服务端文件{0}{1}导出完毕,存放目录为：{2}", fileName, savefile, savepath);
+                    Console.ResetColor(); //将控制台的前景色和背景色设为默认值
+                }
+
+            }
+
         }
         /// <summary>
         /// 导出
